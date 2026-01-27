780d837ddd711da32d57eb07223202efa1c04eed
Philippe GRABARSKY <philippe.grabarsky@amadeus.com>
2026-01-27 13:30:58 +0100

    cdir v1.0.0

--- Formula/cdir.rb
+++ Formula/cdir.rb
@@ -1,8 +1,8 @@
 class Cdir < Formula
     desc "cdir allows you to quickly navigate to a directory recorded into your history"
     homepage "https://github.com/AmadeusITGroup/cdir"    
-    url "https://github.com/AmadeusITGroup/cdir/releases/download/v0.0.11/cdir-aarch64-apple-darwin.tar.gz"
-    sha256 "4b86bb75d0f4ecd334915897f048df04d63f8831e97b490be60ac9f1049322be"
+    url "https://github.com/AmadeusITGroup/cdir/releases/download/v1.0.0/cdir-aarch64-apple-darwin.tar.gz"
+    sha256 "964493568e1119ff7e901d472cc3252e8512a75dc9d1f4b6216c34509859c419"
     license "Apache-2.0 license"
 
 
